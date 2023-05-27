0030106a90ab316387c98f8c4ba2e46b10f29b35
Whynot46 <83154713+Whynot46@users.noreply.github.com>
2023-05-27 04:54:48 +0400

    Create authorization_data.py

---
+++ authorization_data.py
@@ -0,0 +1,7 @@
+token = 'vk1.a.vpBPh4jTyEBeOATnvSQqCoGfnlJKU_zunR8WGrotvTb4Ds2Onf6Dva-KfCjUfCSp6DaD94rllPHPzJ7mXKi31V3MZw3O9lTMbZ1XbscoPXoYM4pFQ0jDm7XQ9EHHNESlYqnXgJu2C6mFYd9mw9ri9T33Aw3-wv-IV5SMXX1pf3mHkd62UIrphfw51jf42IWogFBq3nDVjiPZD0i7_SyzAw'
+owner_id = -144688488
+from_group = 1
+version_vk = '5.131'
+url = 'https://api.vk.com/method/wall.post'
+
+source_group_ids = [-145945161, -219611209]
